6e31ce480ec20fd7db8ce8913434d7f0632bb9e7
Gabriel Martin <gabrielmartin13009@gmail.com>
2024-04-21 18:31:14 +0200

    changed to php

--- header.php
+++ header.php
@@ -8,19 +8,5 @@
     <script src="script.js" defer></script>
     <link rel="stylesheet" href="style.css">
     <link rel="icon" type="image/x-icon" href="./assets/mathilde-icon.PNG">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
 </head>
-<body>
-
-    <h1 class="welcome"><a href="index.html">L'encyclopédie de mathildouce &#x1F63A;</h1></a>
-    <div class="center">
-        <input type="text" class="search-input" placeholder="Cherche un chat !">
-        <button class="search-btn">Search</button>
-    </div>
-
-    <div class="page"></div>
-
-    <img class="chats" src="./assets/chats.jpg" alt="chats">
-    
-</body>
-</html>
